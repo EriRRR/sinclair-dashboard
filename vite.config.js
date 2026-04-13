be55c932108a6e0aaba74e911a60ee9c0126719e
EriRRR <erir_ramirezr@unicah.edu>
2026-04-12 22:37:55 -0600

    cambio para despliegue en vercel

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/sinclair-dashboard/', // DEBE ser el nombre exacto de tu repositorio
+  base: '/',
 })
